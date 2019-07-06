f6e4c75f1102c0c65b27990d3824ca1d831f4e1f
Tim Savage <tim@savage.company>
2019-07-06 19:27:28 +1000

    Broken out get and create methods

--- pyapp_ext/messaging/factory.py
+++ pyapp_ext/messaging/factory.py
@@ -3,23 +3,28 @@ from pyapp.conf.helpers import NamedPluginFactory
 from .bases import MessageQueue, PubSubQueue, AsyncMessageQueue, AsyncPubSubQueue
 
 __all__ = (
-    "message_queue_factory",
-    "pub_sub_queue_factory",
-    "async_message_queue_factory",
-    "async_pub_sub_queue_factory",
+    "message_queue_factory", "get_message_queue",
+    "async_message_queue_factory", "async_get_message_queue",
+    "pub_sub_queue_factory", "get_pub_sub_queue",
+    "async_pub_sub_queue_factory", "async_get_pub_sub_queue",
 )
-
 
 message_queue_factory = NamedPluginFactory[MessageQueue](
     "MESSAGE_QUEUES", abc=MessageQueue
 )
-pub_sub_queue_factory = NamedPluginFactory[PubSubQueue](
-    "PUB_SUB_QUEUES", abc=PubSubQueue
-)
+get_message_queue = message_queue_factory.create
 
 async_message_queue_factory = NamedPluginFactory[AsyncMessageQueue](
     "MESSAGE_QUEUES", abc=AsyncMessageQueue
 )
+async_get_message_queue = async_message_queue_factory.create
+
+pub_sub_queue_factory = NamedPluginFactory[PubSubQueue](
+    "PUB_SUB_QUEUES", abc=PubSubQueue
+)
+get_pub_sub_queue = pub_sub_queue_factory.create
+
 async_pub_sub_queue_factory = NamedPluginFactory[AsyncPubSubQueue](
     "PUB_SUB_QUEUES", abc=AsyncPubSubQueue
 )
+async_get_pub_sub_queue = async_pub_sub_queue_factory.create
