c8403f3fceb177e186c937c34f8c098e6e0bba85
Tim Savage <tim@savage.company>
2019-07-07 01:48:59 +1000

    Added simple send message

--- pyapp_ext/messaging_cli.py
+++ pyapp_ext/messaging_cli.py
@@ -2,7 +2,11 @@
 pyApp - Messaging CLI
 
 """
-from pyapp.app import CommandGroup
+from asyncio import AbstractEventLoop
+from argparse import FileType
+
+from pyapp.app import argument, CommandGroup, CommandOptions
+from pyapp.injection import inject
 
 
 class Extension:
@@ -12,4 +16,23 @@ class Extension:
 
     @staticmethod
     def register_commands(root: CommandGroup):
-        pass
+        group = root.create_command_group("msg-queue")
+
+        @group.command(help_text="Send a message to a Message Queue")
+        @argument("NAME", help_text="Name of queue from config.")
+        @argument("--body", type=FileType("r"))
+        @inject
+        def send(opts: CommandOptions, *, loop: AbstractEventLoop):
+            from .messaging.factory import async_get_message_queue
+
+            async def _send():
+                async with async_get_message_queue(opts.NAME) as queue:
+                    await queue.send()
+
+            loop.run_until_complete(_send())
+
+        @group.command(help_text="Send a message to a Pub/Sub Queue")
+        @argument("NAME", help_text="Name of queue from config.")
+        @argument("--body", type=FileType("r"))
+        def publish(opts: CommandOptions):
+            pass
