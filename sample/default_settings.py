1c30808440cf2ee952ae740c528d3f15e723b4aa
Tim Savage <tim@savage.company>
2019-07-09 00:27:13 +1000

    Added config for AWS

--- sample/default_settings.py
+++ sample/default_settings.py
@@ -3,9 +3,15 @@ AMQP = {"default": {"url": "amqp://admin:123@localhost/"}}
 SEND_MESSAGE_QUEUES = {
     "amqp": (
         "pyapp_ext.aio_pika.queues.MessageSender",
-        {"routing_key": "message-queue"},
+        {"queue_name": "message-queue"},
     ),
-    "sqs": ("pyapp_ext.aiomq.aws.MessageQueue", {"url": "http://"}),
+    "aws": (
+        "pyapp_ext.aiobotocore.queues.MessageSender",
+        {
+            "queue_name": "message-queue",
+            "client_args": {"endpoint_url": "http://localhost:9324"},
+        },
+    ),
 }
 
 RECEIVE_MESSAGE_QUEUES = {
@@ -13,21 +19,27 @@ RECEIVE_MESSAGE_QUEUES = {
         "pyapp_ext.aio_pika.queues.MessageReceiver",
         {"queue_name": "message-queue"},
     ),
-    "sqs": ("pyapp_ext.aiomq.aws.MessageQueue", {"url": "http://"}),
+    "aws": (
+        "pyapp_ext.aiobotocore.queues.MessageReceiver",
+        {
+            "queue_name": "message-queue",
+            "client_args": {"endpoint_url": "http://localhost:9324"},
+        },
+    ),
 }
 
 PUBLISH_MESSAGE_QUEUES = {
     "amqp": (
         "pyapp_ext.aio_pika.queues.MessagePublisher",
-        {"routing_key": "pubsub-queue"},
+        {"queue_name": "pubsub-queue"},
     ),
-    "sqs": ("pyapp_ext.aiomq.aws.PubSubQueue", {"url": "http://"}),
+    "aws": ("pyapp_ext.aiomq.aws.PubSubQueue", {"url": "http://"}),
 }
 
 SUBSCRIBE_MESSAGE_QUEUES = {
     "amqp": (
         "pyapp_ext.aio_pika.queues.MessageSubscriber",
-        {"exchange_name": "pubsub-queue"},
+        {"queue_name": "pubsub-queue"},
     ),
-    "sqs": ("pyapp_ext.aiomq.aws.PubSubQueue", {"url": "http://"}),
+    "aws": ("pyapp_ext.aiomq.aws.PubSubQueue", {"url": "http://"}),
 }
